882894ac02c1eccfb6efdcae93b8a88322af25d1
Ashwin <asbhagat2020@gmail.com>
2024-07-07 23:15:26 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ app.use(cors());
 app.get("/getData", (req, res)=>{
     res.send("this is home route")
 })
+app.get("/", (req, res)=>{
+    res.send("this is home route for sartik sharma")
+})
 
 app.listen(3000, ()=>{
     console.log("server is running at port 3000")
